ef2fd27e89b6430f485bc195b56943486cebda33
KorzenGameDev <KorzenGameDev@gmail.com>
2019-06-18 17:04:11 +0200

    ECS_01

---
+++ EntityComponentSystem/Assets/ECS_01/MoveSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class MoveSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Translation translation, ref SpeedComponent speedComponent) =>{
+            translation.Value.y += speedComponent.moveSpeed * Time.deltaTime;
+            if (translation.Value.y > 8f)
+                speedComponent.moveSpeed = -math.abs(speedComponent.moveSpeed);
+            if (translation.Value.y < -8f)
+                speedComponent.moveSpeed = math.abs(speedComponent.moveSpeed);
+        });
+    }
+}
